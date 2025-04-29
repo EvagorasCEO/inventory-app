f2f5adc12ffbb548cd6c8a54fa8d767e5ba6f8e8
Evagoras <evagoras.christodoulou@gmail.com>
2025-04-29 16:12:33 +0300

    Fix: Host and Port for Render deployment

--- app.py
+++ app.py
@@ -229,5 +229,7 @@ def low_stock():
 with app.app_context():
     db.create_all()
 
+# === RUN APP CORRECTLY ON RENDER ===
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
